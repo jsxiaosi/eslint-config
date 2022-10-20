67f4467630cba08336445dd99cb8aa89e41f1743
SuperCuteXiaoSi <1531733886@qq.com>
2022-10-20 22:09:48 +0800

    feat: ✨ add commlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,12 +1,3 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      ['feature', 'fixbug', 'refactor', 'docs', 'style', 'perf', 'chore', 'revert'],
-    ],
-    'subject-full-stop': [0, 'never'],
-    'subject-case': [0, 'never'],
-  },
+  extends: ['@jsxiaosi/commitlint-config'],
 };
